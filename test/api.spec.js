d1dbdbc05e80fd656963f6f08187400637f9b4d5
Won Gyun Park <park.wongyun@gmail.com>
2020-06-29 18:32:34 -0700

    Added unit tests for api testing

--- test/api.spec.js
+++ test/api.spec.js
@@ -4,35 +4,8 @@ const Listing = require('../database/Listing.js');
 const generateMockData = require('../database/seed.js');
 const app = require('../server/app.js');
 
-// test('api should respond with the right data', async () => {
-//   jest.setTimeout(90000);
-//   // expect.assertions(3);
-//   const testId = '001';
-//   const testListing = new Listing(await generateMockData(testId));
-//   Listing.findOne = ({ listing_id }) => new Promise((resolve, reject) => {
-//     if (listing_id === testId) {
-//       resolve(testListing);
-//     } else {
-//       reject(new Error(`Mock Error From api.spec.js. listing_id : ${listing_id}`));
-//     }
-//   });
-//   const req = request(app);
-//   const promises = [
-//     req.get('/api/001/reservation')
-//       .then((res) => {
-//         expect(res.statusCode).toBe(200);
-//         expect(res.text).toEqual(JSON.stringify(testListing));
-//       }),
-//     req.get('/api/hackreactor12345678910/reservation')
-//       .then((res) => {
-//         expect(res.statusCode).toBe(500);
-//       }),
-//   ];
-//   return Promise.all(promises);
-// });
-
 describe('API GET Request Unit Test ', () => {
-  it('Should return 200 and response when GET request is made with valid end-point and request params', async () => {
+  it('Should return 200 and response when GET request is made with valid request param of 001', async () => {
     jest.setTimeout(90000);
     const testListingOne = new Listing(await generateMockData('001'));
     Listing.findOne = ({ listing_id }) => new Promise((resolve, reject) => {
@@ -45,11 +18,11 @@ describe('API GET Request Unit Test ', () => {
     return request(app).get('/api/001/reservation')
       .then((res) => {
         expect(res.statusCode).toBe(200);
-        expect(res.text).toEqual(JSON.stringify(testListingOne));
+        expect(res.body.listing_id).toBe('001');
       });
   });
 
-  it('Should return 200 and response when GET request is made with valid end-point and request params', async () => {
+  it('Should return 200 and response when GET request is made with valid request param of 010', async () => {
     jest.setTimeout(90000);
     const testListingTen = new Listing(await generateMockData('010'));
     Listing.findOne = ({ listing_id }) => new Promise((resolve, reject) => {
@@ -62,11 +35,16 @@ describe('API GET Request Unit Test ', () => {
     return request(app).get('/api/010/reservation')
       .then((res) => {
         expect(res.statusCode).toBe(200);
-        expect(res.text).toEqual(JSON.stringify(testListingTen));
+        expect(res.body.listing_id).toBe('010');
+        expect(res.body).toHaveProperty('listing_id');
+        expect(res.body).toHaveProperty('guests');
+        expect(res.body).toHaveProperty('open_dates');
+        expect(res.body).toHaveProperty('price');
+        expect(res.body).toHaveProperty('review');
       });
   });
 
-  it('Should return 200 and response when GET request is made with valid end-point and request params', async () => {
+  it('Should return 200 and response when GET request is made with valid request param of 100', async () => {
     jest.setTimeout(90000);
     const testListingHundred = new Listing(await generateMockData('100'));
     Listing.findOne = ({ listing_id }) => new Promise((resolve, reject) => {
@@ -84,26 +62,15 @@ describe('API GET Request Unit Test ', () => {
       });
   });
 
+  it('Should return 500 when invalid GET request with string is made', async () => request(app).get('/api/blueberrySoju/reservation')
+    .then((res) => {
+      expect(res.statusCode).toBe(500);
+    }));
 
-
-
-
-
-
-
-
-
-
-  // it('Should return valid response when valid GET request is made', async () => {
-  //   const responseOne = await request(app).get('/api/001/reservation');
-  //   const responseTen = await request(app).get('/api/010/reservation');
-  //   const responseHundred = await request(app).get('/api/100/reservation');
-
-  //   expect(responseOne.body).toHaveProperty('listing_id');
-  //   expect(responseOne.body).toHaveProperty('guests');
-  //   expect(responseOne.body).toHaveProperty('open_dates');
-  //   expect(responseOne.body).toHaveProperty('price');
-  //   expect(responseOne.body).toHaveProperty('review');
+  it('Should return 500 when invalid GET request with number is made', async () => request(app).get('/api/45647891328/reservation')
+    .then((res) => {
+      expect(res.statusCode).toBe(500);
+    }));
 
   //   expect(responseTen.body).toHaveProperty('listing_id');
   //   expect(responseTen.body).toHaveProperty('guests');
